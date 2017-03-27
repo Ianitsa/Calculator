b78be1091b728baac25f0da8bfd20be4445a025e
Ianitsa <iani_georgieva@mail.bg>
2017-03-27 14:18:46 +0300

    Add files via upload
    Splash Screen form   (winforms c#)

---
+++ Splash Screen App/Splash Screen/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Splash_Screen
+{
+    public partial class FrmSplashScreen : Form
+    {
+        public int timerLeft { get; private set; }
+
+        public FrmSplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (timerLeft > 0)
+            {
+                timerLeft = timerLeft - 1;
+            }
+            else
+            {
+                timer1.Stop();
+                // new MainForm().Show();
+                this.Hide();
+            }
+        }
+
+        private void FrmSplashScreen_Load(object sender, EventArgs e)
+        {
+            timerLeft = 40;
+            timer1.Start();
+            new MainForm().Show();
+        }
+    }
+}
